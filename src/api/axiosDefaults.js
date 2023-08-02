58a443baeac58c29e52c22a6a3ef5d98c8326c04
Andreas Karlsson <andreas.krln@hotmail.com>
2023-08-02 16:13:01 +0000

    Created new api app on heroku

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://explore-sthlm-231a49fd8022.herokuapp.com';
+axios.defaults.baseURL = 'https://explore-sthlm-ffc7a7853efc.herokuapp.com';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
